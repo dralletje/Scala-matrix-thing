032b14eebce68c991a06c99a664f482957f11b76
Dral <m.c.dral@gmail.com>
2014-10-16 07:52:07 +0200

    So much speed, wow

--- Julia/app.jl
+++ Julia/app.jl
@@ -1,4 +1,4 @@
-const si_ze = 10000
+const si_ze = 20000
 const t = Uint8 # Change to Uint16 later.. maybe
 const v = Array{Float32, 1}
 const m = Array{t, 2}
@@ -7,32 +7,15 @@ const m = Array{t, 2}
 
 import functions.jl
 
+
 function things(matrix::m)
-  vector = [1.0f0 for x=1:si_ze]::v
-
-  oldvector = Array(Float32, 1)
-  count = 0
-  while true
-    oldvector = vector
-    vector = normalize(keer(matrix, vector))
-    count += 1
-
-    if count > 3 # lookslike(vector, oldvector)
-      println(count)
-      return map(x -> round(x, 2), vector)
-    end
-  end
-end
-
-function things2(matrix::m)
   vector = [1.0f0 for x=1:si_ze]::v
   newvector = Array(Float32, si_ze)
 
   count = 0
   while true
-    println("\n=====")
-    @time (vector, newvector) = keer!(matrix, vector, newvector)
-    @time normalize!(vector)
+    (vector, newvector) = keer!(matrix, vector, newvector)
+    normalize!(vector)
     count += 1
 
     if count > 3 # lookslike(vector, newvector)
@@ -42,27 +25,4 @@ function things2(matrix::m)
   end
 end
 
-#
-
-function small(matrix::m)
-  vector = [1.0f0 for x=1:si_ze]::v
-  vector = normalize(keer(matrix, vector))
-  return vector
-end
-
-function small2(matrix::m)
-  vector = [1.0f0 for x=1:si_ze]::v
-  newvector = Array(Float32, si_ze)
-  @time (vector, newvector) = keer!(matrix, vector, newvector)
-  println("Calc\n")
-  @time normalize!(vector)
-  println("Normalize\n")
-  return vector
-end
-
-@time v1 = small(matrix)
-@time v2 = small2(matrix)
-
-println("\n")
-println(v1 == v2)
-#@time [things() for i=1:5]
+@time v1 = things(matrix)
--- Julia/functions.jl
+++ Julia/functions.jl
@@ -16,40 +16,20 @@ function lookslike(vec1::v, vec2::v)
   return true
 end
 
-function normalize(vector::v)
-  m, i = findmax(vector)
-  map(x -> x/m, vector)
-end
-
 function normalize!(vector::v)
-  @time m, i = findmax(vector)
-  println("Findmax\n")
+  m, i = findmax(vector)
   for k=1:length(vector)
     vector[k] = vector[k] / m
   end
-  println("Map\n")
   return vector
-end
-
-function keer(matrix::m, row::Integer, vector::v)
-  sum([matrix[i, row]*vector[i] for i=1:size(matrix, 2)])
-end
-
-function keer(matrix::m, vector::v)
-  [keer(matrix, i, vector) for i=1:size(matrix, 1)]
 end
 
 # Mutationing
 function keer!(matrix::m, vector::v, newvector::v)
-  #println("\n=========")
   for i=1:size(matrix, 1)
     newvector[i] = 0.0
-    #println(@sprintf "New row: %d" newvector[i])
     for k=1:size(matrix, 2)
-      #println(newvector[i])
-      #println(@sprintf "Doing %f * %f" matrix[k, i] vector[i])
-      newvector[i] += vector[k]*matrix[k, i]
-      #println(newvector[i])
+      newvector[i] += matrix[k, i]*vector[k]
     end
   end
   return (newvector, vector) # Newvector becomes vector, and vector bcomes space for newvector
